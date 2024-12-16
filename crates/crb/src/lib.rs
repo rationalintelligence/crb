8d21078cfdf62e57a3719391cfac6389be9e3212
Denis Kolodin <deniskolodin@gmail.com>
2024-12-16 20:44:18 +0100

    Reexport crates

--- crates/crb/src/lib.rs
+++ crates/crb/src/lib.rs
@@ -1,1 +1,23 @@
+pub mod actor {
+    pub use crb_actor::*;
+}
 
+pub mod core {
+    pub use crb_core::*;
+}
+
+pub mod routine {
+    pub use crb_routine::*;
+}
+
+pub mod runtime {
+    pub use crb_runtime::*;
+}
+
+pub mod supervisor {
+    pub use crb_supervisor::*;
+}
+
+pub mod task {
+    pub use crb_task::*;
+}

c286800ccf419710fd7fff3be23f78af849be427
Denis Kolodin <deniskolodin@gmail.com>
2024-12-19 22:57:13 +0100

    Limit errors in the Failures struct

--- crates/crb-conductor/src/lib.rs
+++ crates/crb-conductor/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod actor;
 
+pub use actor::ConductedActor;
+
 use crb_actor::Actor;
 
 pub trait Conductor: Actor {}
--- crates/crb-runtime/src/error.rs
+++ crates/crb-runtime/src/error.rs
@@ -1,14 +1,35 @@
 use anyhow::Error;
+use std::collections::VecDeque;
 
-#[derive(Default)]
+const DEFAULT_LIMIT: usize = 8;
+
 pub struct Failures {
-    errors: Vec<Error>,
+    limit: usize,
+    errors: VecDeque<Error>,
+}
+
+impl Failures {
+    pub fn new(limit: usize) -> Self {
+        Self {
+            limit,
+            errors: VecDeque::with_capacity(limit),
+        }
+    }
+}
+
+impl Default for Failures {
+    fn default() -> Self {
+        Self::new(DEFAULT_LIMIT)
+    }
 }
 
 impl Failures {
     pub fn put(&mut self, res: Result<(), Error>) {
+        if self.errors.len() >= self.limit {
+            self.errors.pop_front();
+        }
         if let Err(err) = res {
-            self.errors.push(err);
+            self.errors.push_back(err);
         }
     }
 }

84cd0840a01405f72f945b110e5f2fff865f6afc
Denis Kolodin <deniskolodin@gmail.com>
2024-12-21 19:58:45 +0100

    Add values to the test

--- crates/crb/tests/test_pipeline.rs
+++ crates/crb/tests/test_pipeline.rs
@@ -5,7 +5,9 @@ use crb_pipeline::{AddressExt, ConductedActor, Pipeline};
 use crb_runtime::ManagedContext;
 use tokio::time::{sleep, Duration};
 
-struct FirstProcessor {}
+struct FirstProcessor {
+    value: u16,
+}
 
 #[async_trait]
 impl Actor for FirstProcessor {
@@ -19,19 +21,23 @@ impl Actor for FirstProcessor {
 }
 
 impl ConductedActor for FirstProcessor {
-    type Input = ();
-    type Output = ();
+    type Input = u8;
+    type Output = u16;
 
-    fn input(_input: Self::Input) -> Self {
-        Self {}
+    fn input(input: Self::Input) -> Self {
+        Self {
+            value: input as u16 * 2,
+        }
     }
 
     fn output(&mut self) -> Self::Output {
-        ()
+        self.value
     }
 }
 
-struct SecondProcessor {}
+struct SecondProcessor {
+    value: u32,
+}
 
 #[async_trait]
 impl Actor for SecondProcessor {
@@ -45,15 +51,17 @@ impl Actor for SecondProcessor {
 }
 
 impl ConductedActor for SecondProcessor {
-    type Input = ();
-    type Output = ();
+    type Input = u16;
+    type Output = u32;
 
-    fn input(_input: Self::Input) -> Self {
-        Self {}
+    fn input(input: Self::Input) -> Self {
+        Self {
+            value: input as u32 * 2,
+        }
     }
 
     fn output(&mut self) -> Self::Output {
-        ()
+        self.value
     }
 }
 
@@ -62,14 +70,15 @@ async fn test_pipeline() -> Result<(), Error> {
     let mut pipeline = Pipeline::new();
 
     // Routing
-    pipeline.input::<(), FirstProcessor>();
+    pipeline.input::<u8, FirstProcessor>();
     pipeline.route::<FirstProcessor, SecondProcessor>();
 
     // pipeline.route_map::<FirstProcessor, SecondProcessor>();
     // pipeline.route_split::<FirstProcessor, SecondProcessor>();
+    // pipeline.route_merge::<FirstProcessor, SecondProcessor>();
 
     let mut addr = pipeline.spawn();
-    addr.ingest(())?;
+    addr.ingest(8)?;
     sleep(Duration::from_millis(10)).await;
     addr.interrupt()?;
     addr.join().await?;

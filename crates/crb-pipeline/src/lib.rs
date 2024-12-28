9f79687afe6ede5acadab47e0b4c8bd38c0841bd
Denis Kolodin <deniskolodin@gmail.com>
2024-12-29 00:33:29 +0100

    Deactivate sync and async tasks

--- crates/crb-pipeline/src/lib.rs
+++ crates/crb-pipeline/src/lib.rs
@@ -1,26 +1,30 @@
 pub mod actor;
-pub mod async_task;
 pub mod extension;
-#[cfg(feature = "sync")]
 pub mod hybryd_task;
 pub mod meta;
 pub mod pipeline;
 pub mod routine;
 pub mod service;
 pub mod stage;
+
+/*
 #[cfg(feature = "sync")]
 pub mod sync_task;
+pub mod async_task;
+*/
 
 pub mod kit {
     pub use crate::actor::{stage::Actor, ActorStage};
-    pub use crate::async_task::stage::AsyncTask;
     pub use crate::extension::AddressExt;
-    #[cfg(feature = "sync")]
     pub use crate::hybryd_task::stage::HybrydTask;
     pub use crate::pipeline::Pipeline;
     pub use crate::routine::{stage::Routine, RoutineStage};
     pub use crate::service::{stage::Input, InputStage};
     pub use crate::stage::Stage;
+
+    /*
+    pub use crate::async_task::stage::AsyncTask;
     #[cfg(feature = "sync")]
     pub use crate::sync_task::stage::SyncTask;
+    */
 }

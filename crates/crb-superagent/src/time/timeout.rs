7c1b07d56637eeaa9be251b6bff175e8249ab70c
Denis Kolodin <deniskolodin@gmail.com>
2025-01-16 18:35:33 +0100

    Replace OnTick trait with OnEvent

--- crates/crb-superagent/src/time/timeout.rs
+++ crates/crb-superagent/src/time/timeout.rs
@@ -1,18 +1,12 @@
 use anyhow::Result;
 use async_trait::async_trait;
-use crb_agent::{Agent, AgentSession, Context, DoAsync, MessageFor, Next, RunAgent, ToAddress};
+use crb_agent::{Agent, AgentSession, DoAsync, Next, OnEvent, RunAgent, ToAddress};
 use crb_core::{
     time::{sleep, Duration},
     Slot, SyncTag,
 };
 use crb_runtime::{JobHandle, Task};
-use crb_send::{MessageSender, Sender};
-
-// TODO: Refactor to use `OnEvent`
-#[async_trait]
-pub trait OnTimeout<T = ()>: Agent {
-    async fn on_timeout(&mut self, tag: T, ctx: &mut Context<Self>) -> Result<()>;
-}
+use crb_send::{Recipient, Sender};
 
 pub struct Timeout {
     #[allow(unused)]
@@ -20,15 +14,15 @@ pub struct Timeout {
 }
 
 impl Timeout {
-    pub fn new<A, T>(address: impl ToAddress<A>, duration: Duration, tag: T) -> Self
+    pub fn new<A, T>(address: impl ToAddress<A>, duration: Duration, event: T) -> Self
     where
-        A: OnTimeout<T>,
+        A: OnEvent<T>,
         T: SyncTag,
     {
         let task = TimeoutTask {
             duration,
-            tag: Slot::filled(tag),
-            sender: address.to_address().sender(),
+            event: Slot::filled(event),
+            sender: address.to_address().recipient(),
         };
         let mut job = RunAgent::new(task).spawn().job();
         job.cancel_on_drop(true);
@@ -38,8 +32,8 @@ impl Timeout {
 
 struct TimeoutTask<T> {
     duration: Duration,
-    tag: Slot<T>,
-    sender: MessageSender<Completed<T>>,
+    event: Slot<T>,
+    sender: Recipient<T>,
 }
 
 impl<T> Agent for TimeoutTask<T>
@@ -61,25 +55,8 @@ where
 {
     async fn once(&mut self, _: &mut ()) -> Result<Next<Self>> {
         sleep(self.duration).await;
-        let tick = Completed {
-            tag: self.tag.take()?,
-        };
-        self.sender.send(tick)?;
+        let event = self.event.take()?;
+        self.sender.send(event)?;
         Ok(Next::done())
-    }
-}
-
-struct Completed<T> {
-    tag: T,
-}
-
-#[async_trait]
-impl<A, T> MessageFor<A> for Completed<T>
-where
-    A: OnTimeout<T>,
-    T: SyncTag,
-{
-    async fn handle(self: Box<Self>, agent: &mut A, ctx: &mut Context<A>) -> Result<()> {
-        agent.on_timeout(self.tag, ctx).await
     }
 }

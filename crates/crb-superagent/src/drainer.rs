3d3dfe0c89832ca0117009edba04da0d7de75096
Denis Kolodin <deniskolodin@gmail.com>
2025-01-19 23:53:44 +0100

    Add sync drainer

--- crates/crb-superagent/src/drainer.rs
+++ crates/crb-superagent/src/drainer.rs
@@ -1,6 +1,9 @@
 use crate::routine::{AsyncRoutine, Routine, SyncRoutine};
+use anyhow::Result;
+use async_trait::async_trait;
 use crb_agent::RunAgent;
 use crb_runtime::{JobHandle, Task};
+use crb_send::{Recipient, Sender};
 
 pub struct Drainer {
     #[allow(unused)]
@@ -26,5 +29,35 @@ impl Drainer {
         let mut job = RunAgent::new(routine).spawn().job();
         job.cancel_on_drop(true);
         Self { job: Some(job) }
+    }
+}
+
+struct AsyncDrainer {}
+
+#[async_trait]
+impl AsyncRoutine for AsyncDrainer {
+    async fn routine(&mut self) -> Result<()> {
+        Ok(())
+    }
+}
+
+pub trait SyncDrainerFn<T = ()>: FnMut() -> T + Send + 'static {}
+
+impl<F, T> SyncDrainerFn<T> for F where F: FnMut() -> T + Send + 'static {}
+
+struct SyncDrainer<M> {
+    func: Box<dyn SyncDrainerFn<Result<M>>>,
+    recipient: Recipient<M>,
+}
+
+#[async_trait]
+impl<M> AsyncRoutine for SyncDrainer<M>
+where
+    M: Send + 'static,
+{
+    async fn routine(&mut self) -> Result<()> {
+        let message = (self.func)()?;
+        self.recipient.send(message)?;
+        Ok(())
     }
 }

275c0bd0efc22cbf035ad31a86f7b429a9d06225
Denis Kolodin <deniskolodin@gmail.com>
2025-01-24 07:03:26 +0100

    Refactor examples

--- examples/crates-dumper/src/main.rs
+++ examples/crates-dumper/src/main.rs
@@ -1,9 +1,9 @@
 use anyhow::Result;
-use crb::agent::{RunAgent, Task};
+use crb::agent::Runnable;
 use crb_example_crates_dumper::CratesLoader;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    RunAgent::new(CratesLoader::new()).run().await;
+    CratesLoader::new().run().await;
     Ok(())
 }

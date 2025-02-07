83cc3ee274066fa1b5bada9f4810584b2abf34f5
Denis Kolodin <deniskolodin@gmail.com>
2025-02-07 21:03:54 +0100

    Rename the handle struct to Interval

--- crates/crb-superagent/src/timer/interval.rs
+++ crates/crb-superagent/src/timer/interval.rs
@@ -7,75 +7,86 @@ use std::pin::Pin;
 use std::task::{Context, Poll};
 use std::time::{Duration, Instant};
 
-pub enum ThrottleCommand {
-    SetInterval(Duration),
+pub struct Interval {
+    command_tx: mpsc::UnboundedSender<IntervalCommand>,
+    stream: Option<IntervalStream>,
 }
 
-pub struct ThrottledStream {
-    last_tick: Instant,
-    current_interval: Duration,
-    next_deadline: Instant,
-    sleep: Pin<Box<Sleep>>,
-    command_rx: mpsc::UnboundedReceiver<ThrottleCommand>,
-}
-
-pub struct ThrottledHandle {
-    command_tx: mpsc::UnboundedSender<ThrottleCommand>,
-}
-
-impl ThrottledHandle {
+impl Interval {
     pub fn set_interval(&self, interval: Duration) -> Result<()> {
         self.command_tx
-            .send(ThrottleCommand::SetInterval(interval))
+            .send(IntervalCommand::SetInterval(interval))
             .map_err(|_| anyhow!("Can't set the interval."))
+    }
+
+    pub fn events(&mut self) -> Result<IntervalStream> {
+        self.stream
+            .take()
+            .ok_or_else(|| anyhow!("Interval events stream has detached already."))
     }
 }
 
-impl ThrottledStream {
-    pub fn new() -> (Self, ThrottledHandle) {
+enum IntervalCommand {
+    SetInterval(Duration),
+}
+
+pub struct IntervalStream {
+    current_interval: Duration,
+    last_tick: Instant,
+    next_deadline: Instant,
+    sleep: Pin<Box<Sleep>>,
+    command_rx: mpsc::UnboundedReceiver<IntervalCommand>,
+}
+
+impl Interval {
+    pub fn new() -> Self {
         let initial_interval = Duration::from_secs(1);
         let now = Instant::now();
         let next_deadline = now + initial_interval;
         let sleep = Box::pin(sleep_until(next_deadline.into()));
         let (command_tx, command_rx) = mpsc::unbounded_channel();
-
-        (
-            ThrottledStream {
-                last_tick: now,
-                current_interval: initial_interval,
-                next_deadline,
-                sleep,
-                command_rx,
-            },
-            ThrottledHandle { command_tx },
-        )
+        let stream = IntervalStream {
+            current_interval: initial_interval,
+            last_tick: now,
+            next_deadline,
+            sleep,
+            command_rx,
+        };
+        Interval {
+            command_tx,
+            stream: Some(stream),
+        }
     }
 }
 
-impl Stream for ThrottledStream {
+impl IntervalStream {
+    fn update_deadline(&mut self) {
+        let new_deadline = self.last_tick + self.current_interval;
+        self.next_deadline = new_deadline;
+        self.sleep.as_mut().reset(new_deadline.into());
+    }
+}
+
+impl Stream for IntervalStream {
     type Item = Instant;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Instant>> {
         loop {
             while let Poll::Ready(cmd) = Pin::new(&mut self.command_rx).poll_recv(cx) {
-                if let Some(ThrottleCommand::SetInterval(new_interval)) = cmd {
+                if let Some(IntervalCommand::SetInterval(new_interval)) = cmd {
                     self.current_interval = new_interval;
-                    let new_deadline = self.last_tick + self.current_interval;
-                    self.next_deadline = new_deadline;
-                    self.sleep.as_mut().reset(new_deadline.into());
+                    self.update_deadline();
                 } else {
-                    break;
+                    // The handle was closed
+                    return Poll::Ready(None);
                 }
             }
 
             let now = Instant::now();
             if now >= self.next_deadline {
-                let tick = now;
-                self.last_tick = tick;
-                let new_deadline = tick + self.current_interval;
-                self.next_deadline = new_deadline;
-                self.sleep.as_mut().reset(new_deadline.into());
-                return Poll::Ready(Some(tick));
+                self.last_tick = now;
+                self.update_deadline();
+                return Poll::Ready(Some(now));
             }
 
             match self.sleep.as_mut().poll(cx) {

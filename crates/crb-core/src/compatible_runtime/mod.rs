e7e9874397c5e4a3ad532dbf1b487990eb880df1
Denis Kolodin <deniskolodin@gmail.com>
2025-02-07 19:25:35 +0100

    Add the new interval implementation

--- crates/crb-core/src/compatible_runtime/mod.rs
+++ crates/crb-core/src/compatible_runtime/mod.rs
@@ -2,5 +2,8 @@
 //! and non-WASM environments.
 
 pub use tokio::sync::{self, mpsc, oneshot, watch};
-pub use tokio::{spawn, task::{spawn_local, JoinHandle}};
 pub use tokio::time;
+pub use tokio::{
+    spawn,
+    task::{spawn_local, JoinHandle},
+};
--- crates/crb-superagent/src/timer/interval.rs
+++ crates/crb-superagent/src/timer/interval.rs
@@ -1,13 +1,11 @@
-pub struct Interval {
-}
-
-/*
-use std::pin::Pin;
-use std::time::{Duration, Instant};
+use anyhow::{anyhow, Result};
+use crb_core::mpsc;
+use crb_core::time::{sleep_until, Sleep};
+use futures::Future;
 use futures::Stream;
+use std::pin::Pin;
 use std::task::{Context, Poll};
-use tokio::sync::mpsc;
-use tokio::time::{sleep_until, Sleep};
+use std::time::{Duration, Instant};
 
 pub enum ThrottleCommand {
     SetInterval(Duration),
@@ -26,13 +24,16 @@ pub struct ThrottledHandle {
 }
 
 impl ThrottledHandle {
-    pub fn set_interval(&self, interval: Duration) {
-        let _ = self.command_tx.send(ThrottleCommand::SetInterval(interval));
+    pub fn set_interval(&self, interval: Duration) -> Result<()> {
+        self.command_tx
+            .send(ThrottleCommand::SetInterval(interval))
+            .map_err(|_| anyhow!("Can't set the interval."))
     }
 }
 
 impl ThrottledStream {
-    pub fn new(initial_interval: Duration) -> (Self, ThrottledHandle) {
+    pub fn new() -> (Self, ThrottledHandle) {
+        let initial_interval = Duration::from_secs(1);
         let now = Instant::now();
         let next_deadline = now + initial_interval;
         let sleep = Box::pin(sleep_until(next_deadline.into()));
@@ -59,8 +60,9 @@ impl Stream for ThrottledStream {
             while let Poll::Ready(cmd) = Pin::new(&mut self.command_rx).poll_recv(cx) {
                 if let Some(ThrottleCommand::SetInterval(new_interval)) = cmd {
                     self.current_interval = new_interval;
-                    self.next_deadline = self.last_tick + self.current_interval;
-                    self.sleep.as_mut().reset(self.next_deadline.into());
+                    let new_deadline = self.last_tick + self.current_interval;
+                    self.next_deadline = new_deadline;
+                    self.sleep.as_mut().reset(new_deadline.into());
                 } else {
                     break;
                 }
@@ -70,8 +72,9 @@ impl Stream for ThrottledStream {
             if now >= self.next_deadline {
                 let tick = now;
                 self.last_tick = tick;
-                self.next_deadline = tick + self.current_interval;
-                self.sleep.as_mut().reset(self.next_deadline.into());
+                let new_deadline = tick + self.current_interval;
+                self.next_deadline = new_deadline;
+                self.sleep.as_mut().reset(new_deadline.into());
                 return Poll::Ready(Some(tick));
             }
 
@@ -82,4 +85,3 @@ impl Stream for ThrottledStream {
         }
     }
 }
-*/
